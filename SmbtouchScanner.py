aafe6ebf818c0f723fd38198219ae2661b2495b6
Evil2997 <dima.molchanow2012@gmail.com>
2024-05-30 09:08:21 +0300

    Change structure of code. Update to python3. Add argparse target, output. Full path to data.json.

--- SmbtouchScanner.py
+++ SmbtouchScanner.py
@@ -1,60 +1,50 @@
-
-#
-# Smbtouch Scanner
-# By: 3gstudent
-# License: BSD 3-Clause
-'''
-
-Automatically scan the inner network.
-Use Smbtouch.exe to detect whether the target is vulnerable.
-Protocol: SMB
-Scan port:445
-Note:
-   You can also use protocl NBT and port 139,just change the Smbtouch-1.1.1.xml
-
-You can get Smbtouch.exe from this:
-https://github.com/x0rz/EQGRP_Lost_in_Translation/blob/master/windows/touches/Smbtouch-1.1.1.exe
-
-'''
 import os
 import fileinput
-#Start IP,change it
-BeginIP = '192.168.1.1'
-#End IP,change it
-EndIP = '192.168.1.10'
-#Log file
-fp = open('log.txt', 'w+')
+import argparse
+import json
+import pathlib
+from typing import Final
 
-OldIP = '      <value>127.0.0.1</value>'
-TempIP = OldIP
-IP1 =  BeginIP.split('.')[0]
-IP2 =  BeginIP.split('.')[1]
-IP3 =  BeginIP.split('.')[2]
-IP4 = BeginIP.split('.')[-1]
-EndIP_last = EndIP.split('.')[-1]
-
-for i in range(int(IP4)-1,int(EndIP_last)):
-     ip = str(IP1+'.'+IP2+'.'+IP3+'.'+IP4)
-     int_IP4 = int(IP4)
-     int_IP4 += 1
-     IP4 = str(int_IP4)
-     NewIP= '      <value>'+ip+'</value>'
-     for line in fileinput.input('Smbtouch-1.1.1.xml',inplace=1):  
-     	print line.rstrip().replace(TempIP,NewIP)
-     TempIP = NewIP			     
-     Output = os.popen(r"Smbtouch-1.1.1.exe").read()
-     Output = Output[0:Output.find('<config',1)]
-     fp.writelines(Output)
-     Flag = Output.find('[-] Touch failed')
-     if Flag == -1 :
-	print '[+] Touch success:	' +ip
-     else:  
-	print '[-] Touch failed:	' +ip
-else:
-     fp.close( )     
-     for line in fileinput.input('Smbtouch-1.1.1.xml',inplace=1):  
-     	print line.rstrip().replace(NewIP,OldIP)
+OLD_IP = '      <value>127.0.0.1</value>'
+TEMP_IP = OLD_IP
 
 
+def update_xml(ip, old_ip, new_ip):
+    for line in fileinput.input('Smbtouch-1.1.1.xml', inplace=True):
+        print(line.rstrip().replace(old_ip, new_ip))
 
 
+def run_scan(ip):
+    command = r"Smbtouch-1.1.1.exe"
+    output = os.popen(command).read()
+    output = output.split('<config', 1)[0]
+    return output
+
+
+def main(target_ip, output_file):
+    results = {}
+    new_ip = f'      <value>{target_ip}</value>'
+    update_xml(target_ip, TEMP_IP, new_ip)
+
+    try:
+        output = run_scan(target_ip)
+        results[target_ip] = "Touch success" if '[-] Touch failed' not in output else "Touch failed"
+        print(
+            f"[+] Touch success: {target_ip}" if '[-] Touch failed' not in output else f"[-] Touch failed: {target_ip}")
+    finally:
+        update_xml(target_ip, new_ip, OLD_IP)
+
+    with open(output_file, 'w') as json_file:
+        json.dump(results, json_file, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Automatically scan the target IP using Smbtouch.exe')
+    parser.add_argument('--target', type=str, required=True, help='Target IP address to scan')
+    parser.add_argument('--output', type=str, required=True, default='data.json',
+                        help='Output file to save the results in JSON format')
+    args = parser.parse_args()
+
+    OUTPUT_JSON: Final[pathlib.Path] = pathlib.Path(__file__).parent / args.output
+
+    main(args.target, OUTPUT_JSON)
